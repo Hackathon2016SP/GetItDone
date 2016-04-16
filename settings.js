d725ce732f5be9c9d2c01dbe68e4cdee00c693a4
Patrick Shao <shao.pat@gmail.com>
2016-04-16 01:01:53 -0700

    Fix duration error on alarm

--- settings.js
+++ settings.js
@@ -1,15 +1,17 @@
 $('#settings').submit(function(e){
+  e.preventDefault();
+  
+
   var data = {};
   var duration = $("#duration").val();
   data["popupTimer"] = duration;
   chrome.storage.local.set(data,function(object){console.log(object)});
 
   var alarmParam = {}
-  alarmParam["delayInMinutes"] = duration;
+  alarmParam["delayInMinutes"] = duration/1;
   console.log(alarmParam);
   chrome.alarms.create(alarmParam);
 
-  e.preventDefault();
-  
+
 });
 
