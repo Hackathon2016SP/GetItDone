759ab733ad87f5bb39498a8bf2011de2deb5407f
Patrick Shao <shao.pat@gmail.com>
2016-04-16 02:45:37 -0700

    Work on sum time function

--- content.js
+++ content.js
@@ -25,6 +25,37 @@ function poop(document){
     $(this).text(insert_poop_to_text($(this).text(),0.05));
   });
 }
-// $(document).ready(function () {
-//   poop(document);
-// });
+$(document).ready(function () {
+  // poop(document);
+  sumTime()
+});
+
+var blacklist = ["facebook","twitter","tumblr","youtube","messenger"];
+
+function sumTime(){
+  chrome.storage.local.get(null, function (object) {
+    var totalTime = 10;
+    for (var url in object) {
+      for (var name in blacklist){
+        if (url.includes(name)){
+
+          //kent's code
+
+        }
+      }
+    }
+
+    chrome.storage.local.get("popupTimer", function(result){
+      if (result["popupTimer"] == undefined){
+        return;
+      } else{
+        if (totalTime > result["popupTimer"]){
+          console.log("pooop!");
+          poop(document);
+        }
+      }
+    })
+
+  });
+}
+
