c6011518664017c0b2a3e304000d4dbe47d84c96
ZiqiXiong <zx012013@mymail.pomona.edu>
2016-04-16 04:27:11 -0700

    Fix js bug

--- website_percentage.js
+++ website_percentage.js
@@ -54,7 +54,7 @@ function draw_chart_for_minutes_before(minutes_before) {
             website_percentage(data, "website-chart");
     });
 }
-var minutes = 60;
+var minutes = 10;
 draw_chart_for_minutes_before(minutes);
 $('#span-select').on('change', function() {
   minutes = parseInt(this.value);
