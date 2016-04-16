8037893122eeeb75bccbfb62388ed98d9db14cb3
Patrick Shao <shao.pat@gmail.com>
2016-04-16 03:07:39 -0700

    Implement total time tracking

--- content.js
+++ content.js
@@ -30,17 +30,31 @@ $(document).ready(function () {
   sumTime()
 });
 
-var blacklist = ["facebook","twitter","tumblr","youtube","messenger"];
+var blacklist = ["facebook","twitter","tumblr","youtube","messenger","reddit"];
 
 function sumTime(){
   chrome.storage.local.get(null, function (object) {
-    var totalTime = 10;
+    var totalTime = 0;
+    var data = {};
     for (var url in object) {
-      for (var name in blacklist){
-        if (url.includes(name)){
-
-          //kent's code
-
+      for (var i in blacklist){
+        if (url.includes(blacklist[i])){
+          console.log(blacklist[i]);
+          console.log(object);
+          console.log(url);
+          console.log(object[url]);
+          var data_for_url = JSON.parse(object[url]);
+          var visitTimeTotal = 0;
+          for (var visitTime in data_for_url) {
+              var visitLength = data_for_url[visitTime];
+              var date = new Date(parseInt(visitTime));
+              var currentDate = new Date();
+              if (date > new Date(currentDate.getTime() - 720 * 60 * 1000)) {
+                  visitTimeTotal = visitTimeTotal + visitLength;
+              }
+          }
+          totalTime+=visitTimeTotal;
+          console.log(totalTime);
         }
       }
     }
