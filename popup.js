11b010d3d6555e5db39506bcaff0f7f9a6919fdd
Patrick Shao <shao.pat@gmail.com>
2016-04-21 00:14:09 -0700

    Add total time online

--- popup.js
+++ popup.js
@@ -24,6 +24,7 @@ var blacklist = ["facebook","twitter","tumblr","youtube","messenger","reddit"];
 
 function timeRemaining(){
    chrome.storage.local.get(null, function (object) {
+      var blackListTime = 0;
       var totalTime = 0;
       var data = {};
       for (var url in object) {
@@ -43,16 +44,29 @@ function timeRemaining(){
                     visitTimeTotal = visitTimeTotal + visitLength;
                 }
             }
-            totalTime+=visitTimeTotal;
-          //  console.log(totalTime);
-          }
+            blackListTime+=visitTimeTotal;
+          //  console.log(blackListTime);
+          } 
         }
+
+        //copied of code above, may make it a function later
+        var data_for_url = JSON.parse(object[url]);
+        var visitTimeTotal = 0;
+        for (var visitTime in data_for_url) {
+            var visitLength = data_for_url[visitTime];
+            var date = new Date(parseInt(visitTime));
+            var currentDate = new Date();
+            if (date > new Date(currentDate.getTime() - 1440 * 60 * 1000)) {
+                visitTimeTotal = visitTimeTotal + visitLength;
+            }
+        }
+        totalTime+=visitTimeTotal;
       }
 
       chrome.storage.local.get("popupTimer",function(result){
-        var diff = (result["popupTimer"]*60)-totalTime;
+        var diff = (result["popupTimer"]*60)-blackListTime;
         //$('#remaining').text("Time Left: "+Math.round(diff)+" seconds (~"+Math.round(diff/60)+" mins)");    
-        $('#status').text("Unproductive Time: "+"~"+Math.round(totalTime/60)+" mins (Time Left: ~"+Math.round(diff/60)+" mins)");
+        $('#status').text("Total Time: ~"+Math.round(totalTime/60)+" mins Unproductive Time: ~"+Math.round(blackListTime/60)+" mins (Time Left: ~"+Math.round(diff/60)+" mins)");
       });
 
     });
